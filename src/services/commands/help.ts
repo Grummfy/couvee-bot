e15d551673dbd147204aaaeeff45306b929f3fe7
Grummfy <me@grummfy.be>
2020-05-07 01:32:16 +0200

    fix message length in help

--- src/services/commands/help.ts
+++ src/services/commands/help.ts
@@ -2,17 +2,25 @@ import { Message } from 'discord.js'
 import { injectable } from 'inversify'
 import { CommandAbstract } from '../command-abstract'
 import { NiceMessage } from '../../helper/nice-message'
+import * as _ from 'lodash'
 
 @injectable()
 export class HelpHandler extends CommandAbstract {
     public name = 'help'
 
-    public handle(message: Message): Promise<Message | Message[]> {
+    public async handle(message: Message): Promise<Message | Message[]> {
         let messages: string[] = []
         for (let handler of this.commandHandler.getHandlers()) {
             messages.push(handler.help())
         }
 
-        return message.reply(NiceMessage.wrap(messages.join('\n')))
+        // we can only sendd 1024 character string by message, so we need to split it
+        let chunksOfMessage = _.chunk(messages, 8)
+
+        for (let i = 1; i < chunksOfMessage.length; i++) {
+            await message.reply(NiceMessage.wrap(chunksOfMessage[ i - 1 ].join('\n')))
+        }
+
+        return message.reply(NiceMessage.wrap(chunksOfMessage[ chunksOfMessage.length - 1 ].join('\n')))
     }
 }
